75b53e682d06d5d4763961b471a961c7401aa934
ishakzail <ishak.zail.iz@gmail.com>
2021-12-14 00:30:29 +0100

    V 2.0

--- ft_hex_lower.c
+++ ft_hex_lower.c
@@ -10,4 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+static int ft_len(int num)
+{
+	int	len;
+
+	len = 0;
+	if (num == 0)
+		return (1);
+	while (num != 0)
+	{
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
+
+int    ft_hex_lower(int n)
+{   
+    int     len;
+    char    *hex;
+
+    len = 0;
+    hex = "0123456789abcdef";
+    while (n < 0)
+    {
+        ft_putchar(hex[-n]);
+        n++;
+    }
+    if (n < 16)
+    {
+        len += ft_putchar(hex[n]);
+    }
+    else
+    {
+        ft_hex_lower(n / 16);
+        ft_hex_lower(n % 16);
+    }
+    return (ft_len(n));
+}
